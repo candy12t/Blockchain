53dc15c9f5adb04770aca9734dc612c4100472e0
candy12t <candy12t@gmail.com>
2020-07-03 16:26:24 +0900

    Add: BlockChain Class (#1)

--- blockchain.py
+++ blockchain.py
@@ -1,8 +1,25 @@
 import logging
 import sys
+import time
 
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
-logging.info('test')
-print('test2')
+
+class BlockChain(object):
+
+	def __init__(self):
+		self.transaction_pool = []
+		self.chain = []
+		self.create_block(0, 'init hash')
+
+	def create_block(self, nonce, previous_hash):
+		block = {
+			'timestamp' : time.time(),
+			'transactions': self.transaction_pool,
+			'nonce': nonce,
+			'previous_hash': previous_hash
+		}
+		self.chain.append(block)
+		self.transaction_pool = []
+		return block
