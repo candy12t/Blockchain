85f1d846adc76be4debc3196edb364811cc4e3c0
candy12t <candy12t@gmail.com>
2020-07-04 20:34:51 +0900

    Add: function calculation nonce with proof of work algorithm (#1)

--- blockchain.py
+++ blockchain.py
@@ -6,6 +6,9 @@ import time
 
 import utils
 
+
+MINING_DIFFICULTY = 3
+
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
 
@@ -41,6 +44,23 @@ class BlockChain(object):
 		self.transaction_pool.append(transaction)
 		return True
 
+	def valid_proof(self, transactions, previous_hash, nonce,
+					difficulty=MINING_DIFFICULTY):
+		guess_block = utils.sorted_dict_by_key({
+			'transactions': transactions,
+			'nonce': nonce,
+			'previous_hash': previous_hash
+		})
+		guess_hash = self.hash(guess_block)
+		return guess_hash[:difficulty] == '0'*difficulty
+
+	def proof_of_work(self):
+		transactions = self.transaction_pool.copy()
+		previous_hash = self.hash(self.chain[-1])
+		nonce = 0
+		while self.valid_proof(transactions, previous_hash, nonce) is False:
+			nonce += 1
+		return nonce
 
 
 if __name__ == '__main__':
@@ -49,11 +69,13 @@ if __name__ == '__main__':
 
 	block_chain.add_transaction('A', 'B', 1.0)
 	previous_hash = block_chain.hash(block_chain.chain[-1])
-	block_chain.create_block(5, previous_hash)
+	nonce = block_chain.proof_of_work()
+	block_chain.create_block(nonce, previous_hash)
 	utils.pprint(block_chain.chain)
 
 	block_chain.add_transaction('C', 'D', 2.0)
 	block_chain.add_transaction('X', 'Y', 3.0)
 	previous_hash = block_chain.hash(block_chain.chain[-1])
-	block_chain.create_block(2, previous_hash)
+	nonce = block_chain.proof_of_work()
+	block_chain.create_block(nonce, previous_hash)
 	utils.pprint(block_chain.chain)
