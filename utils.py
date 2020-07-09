ba5509a68f2cbcabdd023a9a367cd33052057e9e
candy12t <candy12t@gmail.com>
2020-07-10 00:36:19 +0900

    Add: find neighbors host function (#4)

--- utils.py
+++ utils.py
@@ -6,6 +6,10 @@ import socket
 
 logger = logging.getLogger(__name__)
 
+# 192.168.0.24 → 192.168.0.24.: prefix_host, 24:last_ip
+RE_IP = re.compile('(?P<prefix_host>^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.)(?P<last_ip>\\d{1,3}$)')
+
+
 def sorted_dict_by_key(unsorted_dict):
 	return collections.OrderedDict(
 		sorted(unsorted_dict.items(), key=lambda d: d[0]))
@@ -42,5 +46,26 @@ def is_found_host(target, port):
 			return False
 
 
+def find_neighbours(my_host, my_port, start_ip_range, end_ip_range, start_port, end_port):
+	# 192.168.0.24 (1,3)
+	address = f'{my_host}:{my_port}'
+	m = RE_IP.search(my_host)
+	if not m:
+		return None
+
+	prefix_host = m.group('prefix_host')
+	last_ip = m.group('last_ip')
+
+	neighbours = []
+	for guess_port in range(start_port, end_port):
+		for ip_range in range(start_ip_range, end_ip_range):
+			guess_host = f'{prefix_host}{int(last_ip)+int(ip_range)}'
+			guess_address = f'{guess_host}:{guess_port}'
+			if is_found_host(guess_host, guess_port) and not guess_address == address:
+				neighbours.append(guess_address)
+	return neighbours
+
+
 if __name__ == '__main__':
-	print(is_found_host('127.0.0.1', 5000))
+	# print(is_found_host('127.0.0.1', 5000))
+	print(find_neighbours('192.168.11.2', 5000, 0, 3, 5000, 5003))
