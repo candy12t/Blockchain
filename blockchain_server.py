9d99d7d65eb214a8c1219b80c3f343472cd3fd5c
candy12t <candy12t@gmail.com>
2020-07-10 10:48:27 +0900

    Add: sync transaction (#4)

--- blockchain_server.py
+++ blockchain_server.py
@@ -22,7 +22,7 @@ def get_blockchain():
 	return cache['blockchain']
 
 
-@app.route('/transactions', methods=['GET', 'POST'])
+@app.route('/transactions', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def transaction():
 	block_chain = get_blockchain()
 	if request.method == 'GET':
@@ -54,6 +54,32 @@ def transaction():
 		if not is_created:
 			return jsonify({'message': 'fail'}), 400
 		return jsonify({'message': 'success'}), 201
+
+	if request.method == 'PUT':
+		request_json = request.json
+		required = (
+			'sender_blockchain_address',
+			'recipient_blockchain_address',
+			'value',
+			'sender_public_key',
+			'signature')
+		if not all(k in request_json for k in required):
+			return jsonify({'message': 'missing values'}), 400
+
+		is_updated = block_chain.add_transaction(
+			request_json['sender_blockchain_address'],
+			request_json['recipient_blockchain_address'],
+			request_json['value'],
+			request_json['sender_public_key'],
+			request_json['signature'],
+		)
+		if not is_updated:
+			return jsonify({'message': 'fail'}), 400
+		return jsonify({'message': 'success'}), 200
+
+	if request.method == 'DELETE':
+		block_chain.transaction_pool = []
+		return jsonify({'message': 'sucess'}), 200
 
 
 @app.route('/chain', methods=['GET'])
